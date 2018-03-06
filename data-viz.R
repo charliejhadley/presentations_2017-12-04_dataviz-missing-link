285c1f613f9b4625a37cd89db61972c6f5457f52
martinjhnhadley <martinjohnhadley@gmail.com>
2018-03-06 12:27:54 +0000

    Code from actual presentation

--- data-viz.R
+++ data-viz.R
@@ -1,1 +1,29 @@
 library("tidyverse")
+library("leaflet")
+
+read.csv()
+
+journeys <- read_csv("data/cheats_journeys.csv")
+
+leaflet() %>%
+  addProviderTiles(providers$Esri.WorldShadedRelief) %>%
+  addCircleMarkers(data = journeys,
+                   lng = ~start.longitude,
+                   lat = ~start.latitude,
+                   color = "green") %>%
+  addCircleMarkers(data = journeys,
+                   lng = ~end.longitude,
+                   lat = ~end.latitude,
+                   color = "red")
+
+gg_routes <- journeys %>%
+  count(start.country, end.country) %>%
+  arrange(desc(n)) %>%
+  mutate(route = paste(start.country,
+                       "->",
+                       end.country)) %>%
+  mutate(route = fct_reorder(route, n)) %>%
+  ggplot(aes(x = route, y = n)) +
+  geom_col() + 
+  coord_flip()
+ggsave("routes-plot.png", gg_routes)
